c09b25d56a963518c2a1caeb375ce87cae7ca763
Kennytian <kt.tian@gmail.com>
2018-02-23 21:54:32 +0800

    Add hello.py file

---
+++ hello.py
@@ -0,0 +1,14 @@
+# -- coding: utf-8 --
+
+print('Hello', 'China')
+print('World')
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+
+a = 'abc'
+b = a
+a = 'xyz'
+print(b)
+
+a = 10
+b = 10 // 3
+print(b)
