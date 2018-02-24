d2c9169c0a142b0baee4ee79724982ec680957d6
Kennytian <kt.tian@gmail.com>
2018-02-24 16:30:02 +0800

    Update hello.py and README.md

--- README.md
+++ README.md
@@ -13,3 +13,8 @@
 10. 在 Python3 环境下安装pip包(不影响Python2)，如：`python3 -m pip install -U pylint`
 11. Python 可以使用 autopep8 来自动排版为 PEP8 风格
 12. 在 Python 文件第一行添加 `# -- coding: utf-8 --`，可防止乱码
+13. Python 的转义字符与其它编程语言一样，用 \ 来表示
+14. Python 允许用r''表示''内部的字符串默认不转义，如：print(r'\\\t\\')-> \\\t\\ 
+15. Python 允许用'''...'''的格式表示多行内容
+16. Python 布尔值只有True、False两种值，布尔值可以用and、or和not运算
+17. 
--- hello.py
+++ hello.py
@@ -12,3 +12,32 @@ print(b)
 a = 10
 b = 10 // 3
 print(b)
+
+print('I\'m ok')
+print('I\'m 学习\nPython')
+
+print('''
+1. 语文
+2. 数学
+3. 英语
+''')
+
+print(1 and 2)  # 2
+print(2 and 1)  # 1
+
+print(True or True)  # True
+print(True or False)  # True
+print(False or False)  # False
+
+print(False or False)  # False
+print(5 > 3 or 1 > 3)  # True
+print(5 > 3 or 1 > 3)  # True
+
+print(not True)  # False
+print(not False)  # True
+print(not 1 > 2)  # True
+
+
+
+
+
