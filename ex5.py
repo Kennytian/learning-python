dce57a5181e98ba0c595a3f8901cc3889e4b2075
Kennytian <kt.tian@gmail.com>
2018-02-25 09:56:31 +0800

    Add some ex files

---
+++ ex5.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+print("习题 5: 更多的变量和打印")
+my_name = "Kenny田"
+my_age = 20
+my_height = 1.73
+my_weight = 76
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+print("Let's talk about %s." % my_name)
+print("He's %s meter tall." % my_height)
+print("He's %s kg heavy." % my_weight)
+print("Actually that's not too heavy.")
+print("He's got %s eyes and %s hair." % (my_eyes, my_hair))
+print("Let's talk about %s." % my_name)
+print("His teeth are usually %s depending on the coffee." % my_teeth)
+print("If I add %d, %s, and %d I get %s." %
+      (my_age, my_height, my_weight, my_age + my_height + my_weight))
