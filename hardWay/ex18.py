cad64947a8ece9bb6f7361923432078163b86b90
Kennytian <kt.tian@gmail.com>
2018-03-09 02:18:51 +0800

    Add a python file

---
+++ hardWay/ex18.py
@@ -0,0 +1,21 @@
+def printTwo(*args):
+    arg1, arg2, = args
+    print('arg1: %r arg2: %r' % (arg1, arg2))
+
+
+def printTwoAgin(arg1, arg2):
+    print('arg1: %r arg2: %r' % (arg1, arg2))
+
+
+def printOne(arg1):
+    print('arg1: %r' % arg1)
+
+
+def printNone():
+    print('I got nothing')
+
+
+printTwo('Kenny', 'Tian')
+printTwoAgin('Jerry', 'Tom')
+printOne('Chin')
+printNone()
